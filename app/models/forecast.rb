1997646c7d328e5d3bf80ffa22101ca1ee7bc0ad
aomnes <omnes.antoine.fr@gmail.com>
2016-10-12 21:14:48 +0000

    temp + summ + wind

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,11 +1,11 @@
 class Forecast < ActiveRecord::Base
-    before_create :forcast_io
+    before_create :forecast_io
   private
 
   def forecast_io
     forecast = ForecastIO.forecast(self.latitude, self.longitude, params: { units: 'si' })
-    self.temperature = forecast.temperature
-    self.summary = forecast.summary
-    self.windspeed = forecast.windspeed
+    self.temperature = forecast.currently.temperature
+    self.summary = forecast.currently.summary
+    self.windSpeed = forecast.currently.windSpeed
   end
 end
